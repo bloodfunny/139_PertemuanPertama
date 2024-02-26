358a2c59fe8e5838598815090cd8605dcf076c2f
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-02-26 11:35:30 +0700

    masukan jejari

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World;" << endl;
-    cout << "hello";
+    // begin
+      // numeric njejari, nluas
+      // display 'masukan jejari= '
+      // accept njejari
+      // compute  nLuas = 3,14 * r * r
+      // display 'luasnya= ' + nluas
+    // end 
+
+    double nJejari, nLuas
 
         system("pause");
 }
